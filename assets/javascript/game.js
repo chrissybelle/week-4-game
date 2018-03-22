6a4218704c77410a43a6f2617a0852d61c64a7c1
cbelle <christabelle.chien@gmail.com>
2018-03-22 18:21:27 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 var goalTotal;
 var goalTotalMin = 19;
 var goalTotalMax = 120;
-var playerTotal;
+var playerTotal = 0;
 var differenceFromGoal;// = goalTotal - playerTotal;
 var wins = 0;
 var losses = 0;
@@ -37,31 +37,31 @@ function randomSushiValue() {
     }
     sushi1Value = Math.floor(Math.random() * (sushiValueMax - sushiValueMin) + sushiValueMin);
     sushiPossibleValues.splice(sushiPossibleValues[sushiPossibleValues.indexOf(sushi1Value)-1],1);
-    console.log(sushi1Value);
-    console.log(sushiPossibleValues);
+    // console.log(sushi1Value);
+    // console.log(sushiPossibleValues);
     sushi2Value = sushiPossibleValues[Math.floor(Math.random() * sushiPossibleValues.length)];
     sushiPossibleValues.splice(sushiPossibleValues.indexOf(sushi2Value),1);
-    console.log(sushi2Value);
-    console.log(sushiPossibleValues);
+    // console.log(sushi2Value);
+    // console.log(sushiPossibleValues);
     sushi3Value = sushiPossibleValues[Math.floor(Math.random() * sushiPossibleValues.length)];
     sushiPossibleValues.splice(sushiPossibleValues.indexOf(sushi3Value),1);
-    console.log(sushi3Value);
-    console.log(sushiPossibleValues);
+    // console.log(sushi3Value);
+    // console.log(sushiPossibleValues);
     sushi4Value = sushiPossibleValues[Math.floor(Math.random() * sushiPossibleValues.length)];
-    console.log(sushi4Value);
-    console.log(sushiPossibleValues);
+    // console.log(sushi4Value);
+    // console.log(sushiPossibleValues);
 }
 
 
 // G A M E P L A Y & E V E N T S
-playerTotal = 0;
+// playerTotal = 0;
 randomGoalTotal();
 randomSushiValue();
 differenceFromGoal = goalTotal - playerTotal;
 
-console.log(goalTotal);
-console.log(playerTotal);
-console.log(differenceFromGoal);
+// console.log(goalTotal);
+// console.log(playerTotal);
+// console.log(differenceFromGoal);
 //if player total exceeds target, restart game
 if (differenceFromGoal < 0) {
     reset();
@@ -82,26 +82,28 @@ if (differenceFromGoal < 0) {
         $("#playerTotal").text("$"+playerTotal);
         differenceFromGoal = goalTotal - playerTotal;
         console.log(differenceFromGoal);
-        console.log(goalTotal);
-        console.log(playerTotal);
+        // console.log(goalTotal);
+        // console.log(playerTotal);
     });
     $("#sushi-2").on("click", function() {
         playerTotal=playerTotal+sushi2Value;
         $("#playerTotal").text("$"+playerTotal);
+        differenceFromGoal = goalTotal - playerTotal;
         console.log(differenceFromGoal);
     });
     $("#sushi-3").on("click", function() {
         playerTotal=playerTotal+sushi3Value;
         $("#playerTotal").text("$"+playerTotal);
+        differenceFromGoal = goalTotal - playerTotal;
         console.log(differenceFromGoal);
     });
     $("#sushi-4").on("click", function() {
         playerTotal=playerTotal+sushi4Value;
         $("#playerTotal").text("$"+playerTotal);
+        differenceFromGoal = goalTotal - playerTotal;
         console.log(differenceFromGoal);
     });
 
-console.log(playerTotal);
 
 }
 
